77f1fb6c9d6ed76b5c5f603c2f7a56a96dad746a
Michael <93446680+darkmeleee@users.noreply.github.com>
2025-01-10 17:58:09 +0300

    fix: ненужный код

--- src/main.py
+++ src/main.py
@@ -4,10 +4,6 @@ import numpy as np
 
 st.title('mindbrain')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
 
 st.audio_input("Записать аудио:")
 st.markdown("<hr>", unsafe_allow_html=True)
